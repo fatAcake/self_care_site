ac5e753e8ece5f0430645f1632910b2eaf9cf949
fatAcake <164332760+fatAcake@users.noreply.github.com>
2025-12-26 00:21:50 +0700

    Add files via upload

---
+++ Self_care/Migrations/20251225171830_InitialCreate.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Self_care.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "articles",
+                columns: table => new
+                {
+                    article_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    content = table.Column<string>(type: "text", nullable: false),
+                    author = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    tags = table.Column<string>(type: "text", nullable: true),
+                    published_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    is_published = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("articles_pkey", x => x.article_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "roles",
+                columns: table => new
+                {
+                    role_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    privilege = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("roles_pkey", x => x.role_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    user_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nickname = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    password = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    registration_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    timezone = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    role_id = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("users_pkey", x => x.user_id);
+                    table.ForeignKey(
+                        name: "users_role_id_fkey",
+                        column: x => x.role_id,
+                        principalTable: "roles",
+                        principalColumn: "role_id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "habits",
+                columns: table => new
+                {
+                    habits_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    user_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("habits_pkey", x => x.habits_id);
+                    table.ForeignKey(
+                        name: "user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "sessions",
+                columns: table => new
+                {
+                    session_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    type = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    duration = table.Column<long>(type: "bigint", nullable: false),
+                    completed = table.Column<bool>(type: "boolean", nullable: true),
+                    notes = table.Column<string>(type: "text", nullable: true),
+                    started_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("sessions_pkey", x => x.session_id);
+                    table.ForeignKey(
+                        name: "sessions_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "user_habits",
+                columns: table => new
+                {
+                    us_habit_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    habit_id = table.Column<int>(type: "integer", nullable: false),
+                    is_marked = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    goal = table.Column<int>(type: "integer", nullable: true),
+                    status = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    frequency = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    execute_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("user_habits_pkey", x => x.us_habit_id);
+                    table.ForeignKey(
+                        name: "user_habits_habit_id_fkey",
+                        column: x => x.habit_id,
+                        principalTable: "habits",
+                        principalColumn: "habits_id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "user_habits_user_id_fkey",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "habit_logs",
+                columns: table => new
+                {
+                    habit_lg_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_habit_id = table.Column<int>(type: "integer", nullable: false),
+                    date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    completed = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false, defaultValueSql: "now()"),
+                    updated_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    deleted = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("habit_logs_pkey", x => x.habit_lg_id);
+                    table.ForeignKey(
+                        name: "habit_logs_user_habit_id_fkey",
+                        column: x => x.user_habit_id,
+                        principalTable: "user_habits",
+                        principalColumn: "us_habit_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "idx_articles_is_published",
+                table: "articles",
+                column: "is_published");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_articles_published_at",
+                table: "articles",
+                column: "published_at");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_habit_logs_date",
+                table: "habit_logs",
+                column: "date");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_habit_logs_user_habit_id",
+                table: "habit_logs",
+                column: "user_habit_id");
+
+            migrationBuilder.CreateIndex(
+                name: "fki_user_id_fkey",
+                table: "habits",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_sessions_started_at",
+                table: "sessions",
+                column: "started_at");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_sessions_type",
+                table: "sessions",
+                column: "type");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_sessions_user_id",
+                table: "sessions",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_user_habits_habit_id",
+                table: "user_habits",
+                column: "habit_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_user_habits_status",
+                table: "user_habits",
+                column: "status");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_user_habits_user_id",
+                table: "user_habits",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_users_email",
+                table: "users",
+                column: "email");
+
+            migrationBuilder.CreateIndex(
+                name: "idx_users_role_id",
+                table: "users",
+                column: "role_id");
+
+            migrationBuilder.CreateIndex(
+                name: "users_email_key",
+                table: "users",
+                column: "email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "articles");
+
+            migrationBuilder.DropTable(
+                name: "habit_logs");
+
+            migrationBuilder.DropTable(
+                name: "sessions");
+
+            migrationBuilder.DropTable(
+                name: "user_habits");
+
+            migrationBuilder.DropTable(
+                name: "habits");
+
+            migrationBuilder.DropTable(
+                name: "users");
+
+            migrationBuilder.DropTable(
+                name: "roles");
+        }
+    }
+}
